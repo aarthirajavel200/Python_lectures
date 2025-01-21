994e3e05b50b0bb11a6f2eed5f0a813699bb5251
aarthirajavel200 <158204438+aarthirajavel200@users.noreply.github.com>
2025-01-21 11:26:29 +0530

    Add files via upload

---
+++ lecvs.py
@@ -0,0 +1,24 @@
+<class 'int'>
+<class 'float'>
+<class 'float'>
+<class 'bool'>
+<class 'bool'>
+123.0
+8
+7.0
+7
+7
+0.0625
+<class 'int'>
+<class 'float'>
+0
+6
+6
+9
+9
+Before swapping
+1
+2
+After swapping
+2
+1
