c4d5d7f5083cd59a486b926b2fa51d5bcd6ce33a
aarthirajavel200 <158204438+aarthirajavel200@users.noreply.github.com>
2025-01-21 11:16:22 +0530

    Add files via upload

---
+++ lect1.py
@@ -0,0 +1,36 @@
+print(type(1234))
+print(type(8.99))
+print(type(9.0))
+print(type(True))
+print(type(False))
+print(float(123))
+print(round(7.9))
+print(float(round(7.2)))
+print(int(7.2))
+print(int(7.9))
+print((13-4)/(12*12))
+print(type(4*3))
+print(type(4.0*3))
+print(int(1/2))
+x = 6 
+print(x)
+_6 =x
+print(_6)
+x = 3+6
+print(x)
+xy = 3+6 
+print(xy)
+y=2
+x = 1
+print("Before swapping")
+print(x)
+print(y)
+
+temp = x
+x =y
+y=temp
+print("After swapping")
+print(x)
+print(y)
+
+
