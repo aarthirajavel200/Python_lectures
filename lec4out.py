08cf287f96c80d6715e0446f9e7edd87743e9579
Aarthi R <158204438+aarthirajavel200@users.noreply.github.com>
2025-02-01 19:29:17 +0530

    Add files via upload

---
+++ lec4out.py
@@ -0,0 +1,29 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: C:/Users/ADMIN/OneDrive/Desktop/VC/lec4.py =============
+aarthi
+5
+0
+2
+4
+0
+2
+4
+6
+8
+2
+8
+-4
+-2
+0
+2
+4
+yes,it's 7
+not found
+not found
+not found
+not found
+not found
+not found
+yes,it's 7
