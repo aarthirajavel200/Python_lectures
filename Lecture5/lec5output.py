543d05b21c1f9e06ea4fa338bd7351de83b03e7c
Aarthi R <158204438+aarthirajavel200@users.noreply.github.com>
2025-02-10 02:26:42 +0530

    Add files via upload

---
+++ Lecture5/lec5output.py
@@ -0,0 +1,13 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:\Users\ADMIN\OneDrive\Desktop\VC\lec5..py =============
+False
+0.9999999999999999 == 1.0
+True
+the square root is 4.8980000000019235
+0.009595999981158343
+Remainder=0.625
+Remainder=0.25
+Remainder=0.5
+the binary representation of the decimal0.625is -125
